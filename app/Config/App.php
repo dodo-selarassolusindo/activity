6adf4190cec12786b28f4e85ec5ae65d1fe11248
dodo-selarassolusindo <119738003+dodo-selarassolusindo@users.noreply.github.com>
2023-04-12 03:16:42 +0700

    .

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://activity.selarassolusindo.com/';
+    public $baseURL = 'https://activity.selarassolusindo.com/public/';
 
     /**
      * --------------------------------------------------------------------------
